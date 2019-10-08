e961abfb7c7f9a93cc9ca00d896fc9fc01ecf06d
Wroud <wrouds@gmail.com>
2019-10-08 18:04:47 +0300

    feat: export IIndependentScope type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { createScope, createIService, IScopeIService } from "./independent";
+export { createScope, createIService, IScopeIService, IIndependentScope } from "./independent";
 export { isScope, Scope, IScope, IService } from "./scope";
 export { isWithScope, withScope, IWithScope } from "./withScope";
 export { createService, IScopeService } from "./createService"
